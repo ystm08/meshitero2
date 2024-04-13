6d4af98b06a70363c836ff62177b70588c3fa128
ystm08 <150111719+ystm08@users.noreply.github.com>
2024-04-13 15:11:19 +0000

    Add active_storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "bfc995ed28c24e278f79094f6d15a2bf.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
