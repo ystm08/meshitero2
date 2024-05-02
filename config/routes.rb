3a5b05ed7c7f15eb7308cc3fe89452b86b63182b
ystm08 <150111719+ystm08@users.noreply.github.com>
2024-05-02 13:19:45 +0000

    Add マイページ編集

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   get "homes/about", as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
 end
