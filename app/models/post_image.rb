03e46277babc98d92a7def3ec2cb4f0fbfe1f2d6
ystm08 <150111719+ystm08@users.noreply.github.com>
2024-05-09 03:15:00 +0000

    Add　権限とバリデーション

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,6 +5,9 @@ class PostImage < ApplicationRecord
   has_many :favorites, dependent: :destroy
   belongs_to :user
 
+  validates :shop_name, presence: true
+  validates :image, presence: true
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
